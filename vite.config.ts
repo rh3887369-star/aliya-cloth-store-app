5cf40f301bd91344516bebe80c91da9778fd58bd
Rahat Hasan <rh3887369@gmail.com>
2026-07-14 17:06:31 +0530

    Add admin and dashboard pages to Vite build

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL("./index.html", import.meta.url)),
+        admin: fileURLToPath(new URL("./admin.html", import.meta.url)),
+        dashboard: fileURLToPath(
+          new URL("./dashboard.html", import.meta.url)
+        ),
+      },
+    },
+  },
+});
